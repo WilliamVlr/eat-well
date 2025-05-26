58465d08aea3ffd8ea68cc4b71c2ad262908ce9c
Navics2512 <ivcornelius@gmail.com>
2025-05-26 12:58:39 +0700

    IVAN PUNYA

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,15 @@ Route::get('/test', function () {
     return view('test');
 });
 
+Route::get('/cateringHomePage', function() {
+    return view('cateringHomePage');
+});
+
+Route::get('/manageCatering', function(){
+    return view('manageCatering');
+});
+
 Route::get('/', function () {
     return view('landingPage');
 });
+
