12967d0d676ab4c0446aebd89aa2621bbe1ad2a2
William Valerie <williamvalerie2@gmail.com>
2025-05-29 22:04:38 +0700

    Revised current active subscription card UI

--- resources/views/customer/home.blade.php
+++ resources/views/customer/home.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Home')
 
 @section('css')
-    @vite(["resources/js/app.js", "resources/sass/app.scss"])
+    @vite(['resources/js/app.js', 'resources/sass/app.scss'])
     <link rel="stylesheet" href="{{ asset('css/customer/home.css') }}">
 @endsection
 
@@ -12,67 +12,87 @@
         <div class="subscription-card">
             <div class="subscription-header">
                 <div class="text-wrapper">My Subscription</div>
-                <div class="div">View All</div>
+                <div class="view-all-wrapper">View All</div>
             </div>
             <div class="subscription-header-2">
                 <div class="subscription-content">
-                <div class="text-wrapper-2">Active From</div>
-                <div class="date-frame"><div class="text-wrapper-3">DD/MM/YYYY</div></div>
+                    <div class="head-wrapper-2">Active From</div>
+                    <div class="sub-frame">
+                        <div class="content-wrapper-3">DD/MM/YYYY</div>
+                    </div>
                 </div>
                 <div class="subscription-content">
-                <div class="text-wrapper-2">Active Until</div>
-                <div class="date-frame"><div class="text-wrapper-3">DD/MM/YYYY</div></div>
+                    <div class="head-wrapper-2">Active Until</div>
+                    <div class="sub-frame">
+                        <div class="content-wrapper-3">DD/MM/YYYY</div>
+                    </div>
                 </div>
             </div>
             <div class="subscription-header-3">
                 <div class="subscription-content">
-                <div class="text-wrapper-2">Nama Penerima</div>
-                <div class="date-frame"><div class="text-wrapper-3">Adit Tolongin Dit</div></div>
+                    <div class="head-wrapper-2">Nama Penerima</div>
+                    <div class="sub-frame">
+                        <div class="content-wrapper-3">Adit Tolongin Dit</div>
+                    </div>
                 </div>
                 <div class="subscription-content-2">
-                <div class="text-wrapper-2">Alamat Pengiriman</div>
-                <div class="date-frame"><p class="p">Jl. Pegangsaan Timur No. 56, Jakarta</p></div>
+                    <div class="head-wrapper-2">Alamat Pengiriman</div>
+                    <div class="sub-frame">
+                        <div class="content-wrapper-3">Jl. Pegangsaan Timur No. 56, Jakarta</div>
+                    </div>
                 </div>
             </div>
             <div class="subscription-header-2">
-                <div class="subscription-content-2"><div class="text-wrapper-4">Catering XYZ Lorem</div></div>
+                <div class="subscription-content-2">
+                    <div class="catering-name">Catering XYZ Lorem</div>
+                </div>
             </div>
-            <div class="frame">
-                <div class="subscription-content-wrapper">
-                <div class="subscription-content-2">
-                    <div class="text-wrapper-5">Breakfast</div>
-                    <div class="paket-n-qty">
-                    <div class="paket">
-                        <div class="text-wrapper-6">Paket A</div>
-                        <div class="text-wrapper-7">Paket B</div>
+            <div class="order-details">
+                <div class="time-slot-container">
+                    <div class="header">
+                        <div class="text-wrapper-5">Breakfast</div>
+                        <div class="text-frame">
+                            <div class="text-wrapper-6">Preparing</div>
+                        </div>
                     </div>
-                    <div class="paket-2">
-                        <div class="text-wrapper-8">1x</div>
-                        <div class="text-wrapper-9">1x</div>
-                    </div>
-                    </div>
-                    <div class="paket-wrapper">
-                    <div class="div-wrapper"><div class="text-wrapper-10">Preparing</div></div>
+                    <div class="paket-list-container">
+                        <div class="paket-container">
+                            <div class="text-wrapper-7">Paket Lorem Ipsum Dolor</div>
+                            <div class="text-wrapper-8">10x</div>
+                        </div>
                     </div>
                 </div>
-                </div>
-                <div class="subscription-content-wrapper">
-                <div class="subscription-content-2">
-                    <div class="text-wrapper-5">Lunch</div>
-                    <div class="paket-n-qty">
-                    <div class="paket"><div class="text-wrapper-6">Paket A</div></div>
-                    <div class="paket-2"><div class="text-wrapper-8">1x</div></div>
+                <div class="time-slot-container-2">
+                    <div class="header">
+                        <div class="text-wrapper-5">Lunch</div>
+                        <div class="text-frame">
+                            <div class="text-wrapper-6">Preparing</div>
+                        </div>
+                    </div>
+                    <div class="paket-list-container-2">
+                        <div class="paket-container">
+                            <p class="text-wrapper-7">Paket Dolor Amet Situ Mang</p>
+                            <div class="text-wrapper-8">2x</div>
+                        </div>
+                        <div class="paket-container">
+                            <p class="text-wrapper-7">Paket Cing Cong Fan Amet Lorem Ipsum</p>
+                            <div class="text-wrapper-9">1x</div>
+                        </div>
                     </div>
                 </div>
-                </div>
-                <div class="subscription-content-wrapper">
-                <div class="subscription-content-2">
-                    <div class="text-wrapper-5">Dinner</div>
-                    <div class="paket-n-qty">
-                    <div class="paket"><div class="text-wrapper-6">Paket B</div></div>
-                    <div class="paket-2"><div class="text-wrapper-8">3x</div></div>
+                <div class="time-slot-container-2">
+                    <div class="header">
+                        <div class="text-wrapper-5">Dinner</div>
+                        <div class="text-frame">
+                            <div class="text-wrapper-6">Preparing</div>
+                        </div>
                     </div>
-                </div>
+                    <div class="paket-list-container">
+                        <div class="paket-container">
+                            <p class="text-wrapper-7">Paket Amet Dolorosa Situtu Puranang</p>
+                            <div class="text-wrapper-9">3x</div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
