9cd1b35e094fda77aa84e4c523958c323a8e209c
William Valerie <williamvalerie2@gmail.com>
2025-06-05 13:44:20 +0700

    Extends master to orderHistory, configure radio CSS in orderHistory.css and make orderHistory js

--- resources/views/customer/orderHistory.blade.php
+++ resources/views/customer/orderHistory.blade.php
@@ -1,3 +1,32 @@
-<div>
-    <!-- Always remember that you are absolutely unique. Just like everyone else. - Margaret Mead -->
-</div>
+@extends('master')
+
+@section('title', 'EatWell | Orders History')
+
+@section('css')
+    @vite(['resources/js/app.js', 'resources/sass/app.scss'])
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+    <link rel="stylesheet" href="{{asset('css/customer/orderHistory.css')}}">
+@endsection
+
+@section('content')
+    <main>
+        <section class="order-history">
+            <div class="container mb-3">
+                <div class="d-flex align-items-center flex-wrap gap-2">
+                    <div class="custom-rating-radio">
+                        <input type="radio" class="form-check-input" name="tabControlHistory" id="allHistory" value="all">
+                        <label for="allOrders" class="form-check-label border d-flex align-items-center justify-content-center">
+                            All
+                        </label>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+@endsection
+
+@section('scripts')
+    
+@endsection
