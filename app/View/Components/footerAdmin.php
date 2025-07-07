417c43e077409352d4f0c7d335d8496b0e1b5eea
William Valerie <williamvalerie2@gmail.com>
2025-07-07 07:33:05 +0700

    Add VendorReviewObserver for create, update, deleted vendorReview to update the vendor's rating and register it to AppServiceProvider

--- app/View/Components/footerAdmin.php
+++ app/View/Components/footerAdmin.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class footer-admin extends Component
+class footerAdmin extends Component
 {
     /**
      * Create a new component instance.
