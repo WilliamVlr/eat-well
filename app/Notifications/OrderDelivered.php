2701f80dcc5346bb795c3389db5c054038b29a6c
micho1216 <michosrian@gmail.com>
2025-07-25 00:12:47 +0700

    Send customer email notification when order status updated

---
+++ app/Notifications/OrderDelivered.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use App\Models\Order;
+
+class OrderDelivered extends Notification
+{
+    use Queueable;
+    
+    protected Order $order;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(Order $order)
+    {
+        $this->order = $order;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail', 'database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject("Order #{{$this->order->orderId}} is delivered")
+            ->greeting('Hello!')
+            ->line("The status of your Order #{{$this->order->orderId}} has been updated. 
+                The vendor is delivering your food. Please kindly wait.")
+            ->line('Thank you for your patience.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'orderId' => $this->order->orderId
+        ];
+    }
+}
---
+++ app/Notifications/OrderPrepared.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use App\Models\Order;
+
+class OrderPrepared extends Notification
+{
+    use Queueable;
+    protected Order $order;
+    
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(Order $order)
+    {
+        $this->order = $order;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail', 'database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->subject("Order #{{$this->order->orderId}} is being prepared.")
+                    ->greeting('Hello!')
+                    ->line("Status update of Order #{{$this->order->orderId}}.
+                        Your food is being prepared right on! Please kindly wait.")
+                    ->line('Thank you for your patience.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'orderId' => $this->order->orderId
+        ];
+    }
+}
