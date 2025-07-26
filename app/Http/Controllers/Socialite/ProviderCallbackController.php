85b10539de1c4574684a6ed98e9b15921d96a689
micho1216 <michosrian@gmail.com>
2025-07-26 15:07:58 +0700

    Fix wrongfully assigned role

--- app/Http/Controllers/Socialite/ProviderCallbackController.php
+++ app/Http/Controllers/Socialite/ProviderCallbackController.php
@@ -2,14 +2,10 @@
 
 namespace App\Http\Controllers\Socialite;
 
-use App\Enums\UserRole;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
-use App\Models\Vendor;
-use Illuminate\Support\Str;
 
 class ProviderCallbackController extends Controller
 {
@@ -18,12 +14,12 @@ class ProviderCallbackController extends Controller
      */
     public function __invoke(string $provider)
     {
-        $role = session()->get('role');
         if(!in_array($provider, ['google'])){
             return redirect(route('register'))->withErrors(['provider'=>'Invalid provider']);
         }
         $socialUser = Socialite::driver($provider)->user();
-
+        
+        $role = session()->get('role');
         $user = User::updateOrCreate([
             'provider_id' => $socialUser->id,
             'provider_name' => $provider,
@@ -32,8 +28,12 @@ class ProviderCallbackController extends Controller
             'email' => $socialUser->email,
             'provider_token' => $socialUser->token,
             'provider_refresh_token' => $socialUser->refreshToken,
-            'role' => Str::ucfirst($role)
         ]);
+
+        if(!$user->role){
+            $user->role = ucfirst($role);
+            $user->save();
+        }
 
         Auth::login($user, true);
 
